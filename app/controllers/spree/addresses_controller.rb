5d0e8c17af0a0dcd56af852b83a3e3f103fee5c0
Željko Jurić <zeljko.juric@inge-mark.hr>
2012-11-25 11:00:44 +0100

    add ability to create address

--- app/controllers/spree/addresses_controller.rb
+++ app/controllers/spree/addresses_controller.rb
@@ -1,32 +1,51 @@
-class Spree::AddressesController < Spree::BaseController
+class Spree::AddressesController < Spree::BaseController  
   helper Spree::AddressesHelper
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  load_and_authorize_resource
   
-  def edit
-    session["user_return_to"] = request.env['HTTP_REFERER']
+  load_and_authorize_resource
+  respond_to :html
+  
+  def new
+    session["user_return_to"] = request.env["HTTP_REFERER"]
   end
   
-  def update
-    if @address.editable?
-      if @address.update_attributes(params[:address])
-        flash[:notice] = I18n.t(:successfully_updated, :resource => I18n.t(:address))
+  def edit
+    session["user_return_to"] = request.env["HTTP_REFERER"]
+  end
+
+  def create
+    @address = Spree::Address.new(params[:address])
+    @address.user = spree_current_user
+    if @address.save
+      respond_with(@address) do |format|
+        format.html { redirect_back_or_default(spree.account_path) }
       end
     else
-      new_address = @address.clone
-      new_address.attributes = params[:address]
-      @address.update_attribute(:deleted_at, Time.now)
-      if new_address.save
-        flash[:notice] = I18n.t(:successfully_updated, :resource => I18n.t(:address))
-      end
+      respond_with(@address)
     end
-    redirect_back_or_default(account_path)
+  end
+
+  def update
+    if @address.editable?
+      @address.attributes = params[:address]
+    else
+      @address.update_attribute(:deleted_at, Time.now)
+      @address = Spree::Address.new(params[:address])
+    end
+    
+    if @address.save
+      respond_with(@address) do |format|
+        format.html { redirect_back_or_default(spree.account_path) }
+      end
+    else
+      respond_with(@address)
+    end
   end
 
   def destroy
     @address.destroy
-
-    flash[:notice] = I18n.t(:successfully_removed, :resource => t(:address))
-    redirect_to(request.env['HTTP_REFERER'] || account_path) unless request.xhr?
+    respond_with(@address) do |format|
+      format.html { redirect_to spree.account_path unless request.xhr? }
+    end
   end
 end
